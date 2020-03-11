635657bc616dfd7cbfcdd7c2b205d7b412e89fad
dm3-1-15 <61787713+dm3-1-15@users.noreply.github.com>
2020-03-11 14:04:45 +0100

    Add files via upload

---
+++ Suma.py
@@ -0,0 +1,6 @@
+def suma(list):
+    suma2 = 0
+    for x in list:
+        suma2 += x
+    print(suma2)
+
---
+++ multi.py
@@ -0,0 +1,5 @@
+def multi(list):
+    multiplication = 0
+    for x in list:
+        multiplication *= x
+    print(multiplication)
